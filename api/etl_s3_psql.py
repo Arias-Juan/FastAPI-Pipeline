47bccc89b70396181e0436f33eee6cacf551010b
Arias-Juan <arias.juan.jose@outlook.com>
2024-11-26 10:32:50 -0300

    fix trigger schema error

--- api/etl_s3_psql.py
+++ api/etl_s3_psql.py
@@ -46,7 +46,7 @@ async def root(request: Request):
         if pandas_df[column].dtype == 'float64':
             pandas_df[column] = pandas_df[column].fillna(0).astype('int64')
     
-    schema_check = schemas.get(table) is not False
+    schema_check = table in schemas
     if schema_check:
         df = spark.createDataFrame(pandas_df, schema=schemas[table])
     else:
